3a382ef3dc88b0aee2b61a18d053a18d6cd8c588
Minji Bang <147597686+BANG-mj@users.noreply.github.com>
2024-09-15 14:41:30 +0900

    Title: 최댓값 구하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/최댓값 구하기/maximum-value.py
+++ 240915/최댓값 구하기/maximum-value.py
@@ -6,9 +6,9 @@ a = int(arr[0])
 b = int(arr[1])
 c = int(arr[2])
 
-if ((a > b) & (a > c)):
+if ((a >= b) & (a >= c)):
     print(a)
-elif ((b > a) & (b > c)):
+elif ((b >= a) & (b >= c)):
     print(b)
 else:
     print(c)
